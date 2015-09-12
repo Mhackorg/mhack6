279763a8def56ff5670c1d7f8c33385ec485249f
ulisrael <ulisrael@gmail.com>
2015-09-12 04:15:45 -0400

    Initial Commit

---
+++ mhacks6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mhacks6
+//
+//  Created by Uriah Israel on 9/12/15.
+//  Copyright (c) 2015 Uriah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
